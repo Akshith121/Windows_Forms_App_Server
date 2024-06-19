91cda99cfab6ad6f0c439a6cdb47149d062ab8b9
Akshith121 <akshithp111@gmail.com>
2024-06-19 15:16:35 +0530

    feat add formRouter file for the server routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import router from './routes/formRouter';
 
 const app = express();
 const PORT = 3000;
 
-app.listen(3000, () => {
+app.use('/', router);
+
+app.listen(PORT, () => {
     console.log("listening!");
 })
